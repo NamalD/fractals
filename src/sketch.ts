4aa0294622b9ad78de0cfd51fa59361d3c6b8184
NamalD <namal@namal.dev>
2021-05-02 19:49:18 +0100

    Setup p5 with TypeScript

---
+++ src/sketch.ts
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  console.log("Se")
+}
+
+function draw() {
+  background(220);
+  ellipse(50,50,80,80);
+}
+
+window.setup = setup;
+window.draw = draw;
